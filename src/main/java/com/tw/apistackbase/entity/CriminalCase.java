71eccbfd4c211731c5fef6aa8f0ef40744bee9bd
xjhxcc <2351889342@qq.com>
2019-07-17 20:14:08 +0800

    Feat:add criminalcase entity and criminalcase repository

---
+++ src/main/java/com/tw/apistackbase/entity/CriminalCase.java
@@ -0,0 +1,41 @@
+package com.tw.apistackbase.entity;
+
+import javax.persistence.*;
+
+@Entity
+public class CriminalCase {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Long time;
+
+    public CriminalCase() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
+}
